2df809431b6a4e4a88f66b63c100e55d30fee453
farzin jimenez <farzinjimenez92@gmail.com>
2023-08-13 13:28:11 -0500

    actualizar

--- js/script.js
+++ js/script.js
@@ -19,7 +19,9 @@ AOS.init({
    delay: 200,
 });
 
-$(document).on("click", '[data-toggle="lightbox"]', function(event) {
-  event.preventDefault();
-  $(this).ekkoLightbox();
-});
+ var angle = 0;
+function galleryspin(sign) { 
+spinner = document.querySelector("#spinner");
+if (!sign) { angle = angle + 45; } else { angle = angle - 45; }
+spinner.setAttribute("style","-webkit-transform: rotateY("+ angle +"deg); -moz-transform: rotateY("+ angle +"deg); transform: rotateY("+ angle +"deg);");
+}
