7fa7a9402e31bdbd84136a3b7719f8f3b6d4f5a9
farzin jimenez <farzinjimenez92@gmail.com>
2023-08-15 22:42:58 -0500

    sin fairebase

--- js/script.js
+++ js/script.js
@@ -1,55 +1,55 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyCJh4_hu-2S_2nmYlY4CETDH0yDbTa-mqY",
-  authDomain: "hydroxpeditions-contact.firebaseapp.com",
-  databaseURL: "https://hydroxpeditions-contact-default-rtdb.firebaseio.com",
-  projectId: "hydroxpeditions-contact",
-  storageBucket: "hydroxpeditions-contact.appspot.com",
-  messagingSenderId: "543977525459",
-  appId: "1:543977525459:web:6a88cb9e8f9deb4a8b8426",
-  measurementId: "G-CD9BEZLYN6",
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCJh4_hu-2S_2nmYlY4CETDH0yDbTa-mqY",
+//   authDomain: "hydroxpeditions-contact.firebaseapp.com",
+//   databaseURL: "https://hydroxpeditions-contact-default-rtdb.firebaseio.com",
+//   projectId: "hydroxpeditions-contact",
+//   storageBucket: "hydroxpeditions-contact.appspot.com",
+//   messagingSenderId: "543977525459",
+//   appId: "1:543977525459:web:6a88cb9e8f9deb4a8b8426",
+//   measurementId: "G-CD9BEZLYN6",
+// };
 
-// // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-const contactFormDB = firebase.database().ref("hydroContact");
+// // // Initialize Firebase
+// firebase.initializeApp(firebaseConfig);
+// const contactFormDB = firebase.database().ref("hydroContact");
 
-document.getElementById("hydroContact").addEventListener("submit", submitForm);
+// document.getElementById("hydroContact").addEventListener("submit", submitForm);
 
-function submitForm(e) {
-  e.preventDefault();
+// function submitForm(e) {
+//   e.preventDefault();
 
-  const name = getElementVal("name");
-  const email = getElementVal("email");
-  const number = getElementVal("number");
-  const massage = getElementVal("message");
+//   const name = getElementVal("name");
+//   const email = getElementVal("email");
+//   const number = getElementVal("number");
+//   const massage = getElementVal("message");
 
-  console.log(name, email, number, massage);
+//   console.log(name, email, number, massage);
 
-  saveMessages(name, email, number, massage);
+//   saveMessages(name, email, number, massage);
 
-  const alertBox = document.querySelector(".alert");
-  alertBox.style.display = "block";
+//   const alertBox = document.querySelector(".alert");
+//   alertBox.style.display = "block";
 
-  setTimeout(() => {
-    alertBox.style.display = "none";
-  }, 3000);
+//   setTimeout(() => {
+//     alertBox.style.display = "none";
+//   }, 3000);
 
-  document.getElementById("hydroContact").reset();
-}
+//   document.getElementById("hydroContact").reset();
+// }
 
-function saveMessages(name, email, number, massage) {
-  const newContactForm = contactFormDB.push();
-  newContactForm.set({
-    name,
-    email,
-    number,
-    massage,
-  });
-}
+// function saveMessages(name, email, number, massage) {
+//   const newContactForm = contactFormDB.push();
+//   newContactForm.set({
+//     name,
+//     email,
+//     number,
+//     massage,
+//   });
+// }
 
-function getElementVal(id) {
-  return document.getElementById(id).value;
-}
+// function getElementVal(id) {
+//   return document.getElementById(id).value;
+// }
 
 let navbar = document.querySelector(".header .flex .navbar");
 
