afa7fffd193af5422225f8eb75ddee80bc6f39de
farzin jimenez <farzinjimenez92@gmail.com>
2023-08-15 21:04:14 -0500

    firebase

--- js/script.js
+++ js/script.js
@@ -1,21 +1,74 @@
-let navbar = document.querySelector('.header .flex .navbar');
+const firebaseConfig = {
+  apiKey: "AIzaSyCJh4_hu-2S_2nmYlY4CETDH0yDbTa-mqY",
+  authDomain: "hydroxpeditions-contact.firebaseapp.com",
+  databaseURL: "https://hydroxpeditions-contact-default-rtdb.firebaseio.com",
+  projectId: "hydroxpeditions-contact",
+  storageBucket: "hydroxpeditions-contact.appspot.com",
+  messagingSenderId: "543977525459",
+  appId: "1:543977525459:web:6a88cb9e8f9deb4a8b8426",
+  measurementId: "G-CD9BEZLYN6",
+};
 
-document.querySelector('#menu-btn').onclick = () =>{
-   navbar.classList.toggle('active');
+// // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const contactFormDB = firebase.database().ref("hydroContact");
+
+document.getElementById("hydroContact").addEventListener("submit", submitForm);
+
+function submitForm(e) {
+  e.preventDefault();
+
+  const name = getElementVal("name");
+  const email = getElementVal("email");
+  const number = getElementVal("number");
+  const massage = getElementVal("message");
+
+  console.log(name, email, number, massage);
+
+  saveMessages(name, email, number, massage);
+
+  const alertBox = document.querySelector(".alert");
+  alertBox.style.display = "block";
+
+  setTimeout(() => {
+    alertBox.style.display = "none";
+  }, 3000);
+
+  document.getElementById("hydroContact").reset();
 }
 
-window.onscroll = () =>{
-   navbar.classList.remove('active');
+function saveMessages(name, email, number, massage) {
+  const newContactForm = contactFormDB.push();
+  newContactForm.set({
+    name,
+    email,
+    number,
+    massage,
+  });
 }
 
-document.querySelectorAll('input[type="number"]').forEach(inputNumber => {
-   inputNumber.oninput = () =>{
-      if(inputNumber.value.length > inputNumber.maxLength) inputNumber.value = inputNumber.value.slice(0, inputNumber.maxLength);
-   };
+function getElementVal(id) {
+  return document.getElementById(id).value;
+}
+
+let navbar = document.querySelector(".header .flex .navbar");
+
+document.querySelector("#menu-btn").onclick = () => {
+  navbar.classList.toggle("active");
+};
+
+window.onscroll = () => {
+  navbar.classList.remove("active");
+};
+
+document.querySelectorAll('input[type="number"]').forEach((inputNumber) => {
+  inputNumber.oninput = () => {
+    if (inputNumber.value.length > inputNumber.maxLength)
+      inputNumber.value = inputNumber.value.slice(0, inputNumber.maxLength);
+  };
 });
 
 AOS.init({
-   duration: 400,
-   delay: 200,
+  duration: 400,
+  delay: 200,
 });
-
