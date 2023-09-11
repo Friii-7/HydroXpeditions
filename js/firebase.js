cd6c720b292e0e845e224ad32192565c808ea9b0
farzin jimenez <farzinjimenez92@gmail.com>
2023-09-10 20:39:01 -0500

    form

--- js/firebase.js
+++ js/firebase.js
@@ -37,7 +37,7 @@ function submitForm(e) {
     alertBox.style.display = "none";
   }, 3000);
 
-  document.getElementById("hydroContact").reset();
+  document.getElementById("form").reset();
 }
 
 function saveMessages(name, email, number, massage, location, date1, guests) {
